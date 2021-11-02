bec1d8ea565ea8215ba4de6c5b518c20256738fb
Kollurupranathi <89772954+Kollurupranathi@users.noreply.github.com>
2021-11-02 12:05:18 +0530

    Update hw6_social.py

--- hw6_social.py
+++ hw6_social.py
@@ -223,7 +223,7 @@ def getHashtagRates(data):
     tempo={}
     for x in data['hashtags']:
         for j in x:
-            if (j not in tempo):
+            if j not in tempo:
                 tempo[j]=0
             tempo[j]+=1
     return tempo
